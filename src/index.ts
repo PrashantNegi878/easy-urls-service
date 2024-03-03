8ba5bd8ec262125d3285ad1a1badf4ae6572c76c
Your Name <you@example.com>
2024-03-03 15:26:45 +0530

    cors allows all

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ connectToDb();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin:['https://easy-urls-ui.vercel.app/','http://localhost:5173/'],
+    origin:"*",
     credentials:true
 }))
 
